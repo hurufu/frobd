1cbc91b292c5b472f139b0e934c5449d51d380d1
Aleksy Grabowski <hurufu@gmail.com>
2023-01-08 10:54:20 +0100

    Random changes

--- main.c
+++ main.c
@@ -11,29 +11,19 @@ int main() {
     };
     int poll_res;
     while ((poll_res = poll(pf, elementsof(pf), 12 * 1000)) > 0) {
-        if (pf[0].revents & POLLNVAL) {
+        if (pf[0].revents & POLLNVAL)
             break;
-        }
-        if (pf[0].revents & POLLHUP) {
+        if (pf[0].revents & POLLHUP)
             break;
-        }
         if (pf[0].revents & POLLRDNORM) {
             unsigned char buf[1];
             const ssize_t s = read(pf[0].fd, buf, sizeof buf);
-            if (s <= 0) {
+            if (s <= 0)
                 break;
-            }
             frob_process_ecr_eft_input(s, buf);
         }
     }
-    for (size_t i = 0; i < elementsof(pf); i++) {
+    for (size_t i = 0; i < elementsof(pf); i++)
         close(pf[i].fd);
-    }
-    switch (poll_res) {
-        case 0:
-            return 1;
-        case -1:
-            return 2;
-    }
-    return 0;
+    return poll_res == 0 ? 1 : poll_res < 0 ? 2 : 0;
 }
