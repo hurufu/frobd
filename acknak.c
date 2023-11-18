90bd1dc6e6ed070a54ea1a835049408585eddb7e
Aleksy Grabowski <hurufu@gmail.com>
2023-11-18 19:11:37 +0100

    Move code to a function

--- acknak.c
+++ acknak.c
@@ -10,7 +10,7 @@ struct select_params {
     const struct timeval deadline;
 };
 
-typedef int (* const io_wait_func)(struct select_params*);
+typedef int (* const io_wait_f)(struct select_params*);
 
 static inline int iselect(struct select_params* const iop, struct timeval* const deadline) {
     return xselect(iop->maxfd, &iop->r, &iop->w, &iop->e, deadline);
@@ -36,7 +36,7 @@ static int io_wait_timeout(struct select_params* const iop) {
 
 static void event_loop(struct select_params* const iop) {
     char buf[1024], * cur = buf;
-    const io_wait_func io_wait = iop->deadline.tv_sec < 0 ? io_wait_indefinitely : (iop->deadline.tv_sec > 0 ? io_wait_timeout : io_nowait);
+    io_wait_f io_wait = iop->deadline.tv_sec < 0 ? io_wait_indefinitely : (iop->deadline.tv_sec > 0 ? io_wait_timeout : io_nowait);
     FD_SET(STDIN_FILENO, &iop->r);
     while ((*io_wait)(iop)) {
         if (FD_ISSET(STDOUT_FILENO, &iop->w)) {
@@ -61,17 +61,21 @@ static void event_loop(struct select_params* const iop) {
     LOGE("End");
 }
 
-int main(const int ac, const char* av[static const ac]) {
-    init_log();
-    struct timeval tmp = { .tv_sec = (ac == 2 ? atoi(av[1]) : 3), .tv_usec = 0 };
+static struct timeval comp_deadline(const int relative) {
+    struct timeval tmp = { .tv_sec = relative, .tv_usec = 0 };
     if (tmp.tv_sec > 0) {
         struct timeval now;
         gettimeofday(&now, NULL);
         timeradd(&now, &tmp, &tmp);
     }
-    struct select_params iop = {
+    return tmp;
+}
+
+int main(const int ac, const char* av[static const ac]) {
+    init_log();
+        struct select_params iop = {
         .maxfd = 3,
-        .deadline = tmp
+        .deadline = comp_deadline(ac == 2 ? atoi(av[1]) : -1)
     };
     FD_ZERO(&iop.r);
     FD_ZERO(&iop.w);
